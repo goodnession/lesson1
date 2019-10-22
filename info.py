393cb0e2240e6ce0b22edf055cae1644081bcd00
Uvarov Artem <goodnession1@gmail.com>
2019-10-22 20:52:33 +0200

    First commit

---
+++ info.py
@@ -0,0 +1,2 @@
+user_info = {'first_name' : 'Artem', 'last_name' : 'Uvarov'}
+print(user_info['first_name'])
---
+++ ld.py
@@ -0,0 +1,17 @@
+array = [3, 5, 7, 9, 10.5]
+
+print(array)
+
+array.append('Python')
+
+print(len(array))
+
+print(array[0])
+
+print(array[len(array)-1])
+
+print(array[1:4])
+
+del array[len(array)-1]
+
+print(array)
