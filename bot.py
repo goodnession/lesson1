97456879f212b720be62789937807aa61c37b846
Uvarov Artem <goodnession1@gmail.com>
2020-01-09 21:18:59 +0500

    Добавил эмодзи

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ import logging
 from random import choice
 
 
+from emoji import emojize
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 
@@ -20,10 +21,10 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
 def main():
     mybot = Updater(settings.API_KEY, request_kwargs=settings.REQUEST_KWARGS)
     dp = mybot.dispatcher
-    dp.add_handler(CommandHandler('start', greet_user))
+    dp.add_handler(CommandHandler('start', greet_user, pass_user_data = True))
     dp.add_handler(CommandHandler('planet', astrology))
     dp.add_handler(CommandHandler('rk', send_rk_pictures))
-    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me, pass_user_data = True))
     mybot.start_polling()
     mybot.idle()
 
@@ -34,14 +35,17 @@ def send_rk_pictures(bot, update):
     bot.send_photo(chat_id = update.message.chat_id, photo = open(rk_pic, 'rb'))
 
 
-def greet_user(bot, update):
-    greeting = ('Приветствую!')
+def greet_user(bot, update, user_data):
+    emoji = get_user_emoji(user_data)
+    user_data['emoji'] = emoji
+    greeting = ('Приветствую! {}'.format(emoji))
     logging.info(greeting)
     update.message.reply_text(greeting)
 
 
-def talk_to_me(bot, update):
-    user_text = 'Привет {}! Ты написал(а): "{}"'.format(update.message.chat.first_name, update.message.text)
+def talk_to_me(bot, update, user_data):
+    emoji = get_user_emoji(user_data)
+    user_text = 'Привет {} {}! Ты написал(а): "{}"'.format(update.message.chat.first_name, emoji, update.message.text)
     logging.info('User: %s, Chat_Id: %s, Message: %s', update.message.chat.username, update.message.chat.id,
                 update.message.text)
     update.message.reply_text(user_text)
@@ -57,6 +61,14 @@ def astrology(bot, update):
         mars = ephem.Mars(date)
         position = ephem.constellation(mars)
     update.message.reply_text(position)
+
+
+def get_user_emoji(user_data):
+    if 'emoji' in user_data:
+        return user_data['emoji']
+    else:
+        user_data['emoji'] = emojize(choice(settings.USER_EMOJI), use_aliases = True)
+        return user_data['emoji']
 
 
 main()
